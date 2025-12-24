a61a39096f6d1f524b462ee3f5a8437e7e38d8ca
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-12-24 12:31:22 -0600

    feat: add initial event classes for PartyAnimals API

---
+++ src/main/java/com/muhdfdeen/partyanimals/api/event/PartyAnimalsEvent.java
@@ -0,0 +1,5 @@
+package com.muhdfdeen.partyanimals.api.event;
+
+public class PartyAnimalsEvent {
+    
+}
---
+++ src/main/java/com/muhdfdeen/partyanimals/api/event/pinata/PinataHitEvent.java
@@ -0,0 +1,5 @@
+package com.muhdfdeen.partyanimals.api.event.pinata;
+
+public class PinataHitEvent {
+    
+}
