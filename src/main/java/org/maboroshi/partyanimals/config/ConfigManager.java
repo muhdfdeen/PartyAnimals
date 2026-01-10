98b8f0bc4b1d77c8e6286f707bccbb2943138dc6
Muhammad Fardeen <m.fdeen02@gmail.com>
2026-01-09 19:36:43 -0600

    fix: use consistent properties for saving configurations

--- src/main/java/org/maboroshi/partyanimals/config/ConfigManager.java
+++ src/main/java/org/maboroshi/partyanimals/config/ConfigManager.java
@@ -1,6 +1,8 @@
 package org.maboroshi.partyanimals.config;
 
 import de.exlll.configlib.ConfigLib;
+import de.exlll.configlib.NameFormatters;
+import de.exlll.configlib.YamlConfigurationProperties;
 import de.exlll.configlib.YamlConfigurations;
 import java.io.File;
 import java.nio.file.Path;
@@ -15,12 +17,15 @@ import org.maboroshi.partyanimals.config.settings.PinataConfig.PinataConfigurati
 
 public class ConfigManager {
     private final File dataFolder;
-
     private final PartyAnimals plugin;
 
     private MainConfiguration mainConfig;
     private Map<String, PinataConfiguration> pinataConfigs;
     private MessageConfiguration messageConfig;
+
+    private static final YamlConfigurationProperties PROPERTIES = ConfigLib.BUKKIT_DEFAULT_PROPERTIES.toBuilder()
+            .setNameFormatter(NameFormatters.LOWER_KEBAB_CASE)
+            .build();
 
     public ConfigManager(PartyAnimals plugin, File dataFolder) {
         this.dataFolder = dataFolder;
@@ -71,18 +76,17 @@ public class ConfigManager {
 
     public void saveConfig() {
         Path settingsPath = new File(dataFolder, "config.yml").toPath();
-        YamlConfigurations.save(settingsPath, MainConfiguration.class, mainConfig, ConfigLib.BUKKIT_DEFAULT_PROPERTIES);
+        YamlConfigurations.save(settingsPath, MainConfiguration.class, mainConfig, PROPERTIES);
 
         for (Map.Entry<String, PinataConfiguration> entry : pinataConfigs.entrySet()) {
             Path path = new File(dataFolder, "pinatas/" + entry.getKey() + ".yml").toPath();
-            YamlConfigurations.save(
-                    path, PinataConfiguration.class, entry.getValue(), ConfigLib.BUKKIT_DEFAULT_PROPERTIES);
+            YamlConfigurations.save(path, PinataConfiguration.class, entry.getValue(), PROPERTIES);
         }
     }
 
     public void saveMessages() {
         Path path = new File(dataFolder, "messages.yml").toPath();
-        YamlConfigurations.save(path, MessageConfiguration.class, messageConfig, ConfigLib.BUKKIT_DEFAULT_PROPERTIES);
+        YamlConfigurations.save(path, MessageConfiguration.class, messageConfig, PROPERTIES);
     }
 
     public MainConfiguration getMainConfig() {
